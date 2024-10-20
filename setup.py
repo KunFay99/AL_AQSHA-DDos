6495e4533aafa1d08fc3eb41d78ec7d878403ddf
KunFay99 <kaffayukiffu@gmail.com>
2024-10-20 11:47:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,16 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 c = input(">>>: ")
 if c == "0":
     os.system("pkg install python")
+    os.system("pkg install python2")
     os.system("pkg install python3")
-    os.system("python3 -m pip install --upgrade-pip")
-    os.system("pip install colorama")
-    os.system("pip install tqdm")
+    os.system("pip install slowloris")
    
 
 elif c == "1":
     os.system("pkg install python")
+    os.system("pkg install python2")
     os.system("pkg install python3")
-    os.system("python3 -m pip install --upgrade-pip")
-    os.system("pip install colorama")
-    os.system("pip install tqdm")
+    os.system("pip install slowloris")
+   
    
 print("Done.")
