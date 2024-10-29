cbd0ef4a9518947a6ac0b79d4dc25447f0c94505
KunFay99 <kaffayukiffu@gmail.com>
2024-10-29 17:54:09 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 
 c = input(">>>: ")
 if c == "0":
-    os.system("pkg install python")
-    os.system("pkg install python2")
-    os.system("pkg install python3")
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("apt install python3")
     os.system("pip install slowloris")
    
 
 elif c == "1":
-    os.system("pkg install python")
-    os.system("pkg install python2")
-    os.system("pkg install python3")
-    os.system("pip install slowloris")
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("apt install python3")
+    os.system("pip3 install slowloris")
    
    
 print("Done.")
